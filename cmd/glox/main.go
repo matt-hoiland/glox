7fd03577c804a7f0a0bb85f2ed14157ccb39e33a
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2025-03-09 17:23:38 -0600

    Clean up main.go

--- cmd/glox/main.go
+++ cmd/glox/main.go
@@ -9,35 +9,34 @@ import (
 	"github.com/matt-hoiland/glox/internal/scanner"
 )
 
-const ExitUsage = 64
-
-var hadError bool
-
 func main() {
 	args := os.Args[1:]
 
+	var err error
 	if len(args) > 1 {
 		fmt.Println("Usage: glox [script]")
 		os.Exit(exit.Usage)
 	} else if len(args) == 1 {
-		if err := runFile(args[0]); err != nil {
-			Error(0, err.Error())
-			os.Exit(exit.DataErr)
-		}
+		err = runFile(args[0])
 	} else {
-		runPrompt()
+		err = runPrompt()
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(exit.DataErr)
 	}
 }
 
-func run(code string) {
+func run(code string) error {
 	scanner := scanner.New(code)
 	tokens, err := scanner.ScanTokens()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		return err
 	}
 	for _, token := range tokens {
 		fmt.Println(token.String())
 	}
+	return nil
 }
 
 func runFile(filename string) (err error) {
@@ -45,16 +44,10 @@ func runFile(filename string) (err error) {
 	if data, err = os.ReadFile(filename); err != nil {
 		return fmt.Errorf("could not read file '%s': %w", filename, err)
 	}
-	run(string(data))
-
-	// Indicate an error in the exit code.
-	if hadError {
-		os.Exit(exit.DataErr)
-	}
-	return nil
+	return run(string(data))
 }
 
-func runPrompt() {
+func runPrompt() error {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("> ")
@@ -62,15 +55,9 @@ func runPrompt() {
 			break
 		}
 		line := reader.Text()
-		run(line)
+		if err := run(line); err != nil {
+			return err
+		}
 	}
-}
-
-func Error(line int, message string) {
-	report(line, "", message)
-}
-
-func report(line int, where, message string) {
-	fmt.Fprintf(os.Stderr, "[line %d] Error%s: %s\n", line, where, message)
-	hadError = true
+	return nil
 }
